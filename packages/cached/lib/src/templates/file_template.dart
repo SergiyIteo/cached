ad4d1e20578f2536334339e0259836b5ad3146e4
svergun_iteo <sergiy.vergun@iteo.com>
2022-05-18 11:55:41 +0200

    Fix typo.

--- packages/cached/lib/src/templates/file_template.dart
+++ packages/cached/lib/src/templates/file_template.dart
@@ -2,8 +2,9 @@ import 'package:cached/src/models/class_with_cache.dart';
 import 'package:cached/src/templates/class_template.dart';
 import 'package:cached/src/templates/interface_template.dart';
 
-class FileTemeplate {
-  FileTemeplate(ClassWithCache classWithCache)
+
+class FileTemplate {
+  FileTemplate(ClassWithCache classWithCache)
       : mixinTemplate = InterfaceTemplate(classWithCache),
         classTemplate = ClassTemplate(classWithCache);
 
